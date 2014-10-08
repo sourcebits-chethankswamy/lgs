62df10cc9888147f30528901172a630a63ed9983
Chethank <chethan.krishnaswamy@sourcebits.com>
2014-10-08 18:44:09 +0530

    Unused files removed

--- application/views/footer_view.php
+++
@@ -1,6 +0,0 @@
-   <!-- Placed at the end of the document so the pages load faster -->
-    <script src="<?php echo SITEURL.'/js/jquery.js'; ?>"></script>
-    <script src="<?php echo SITEURL.'/js/bootstrap.min.js'; ?>"></script>
-    <script src="<?php echo SITEURL.'/js/main.js'; ?>"></script>
-</body>
-</html>
