d05eadcbf38fc567b227deebbab70de1f8e60187
sourcebits-himashuarora <himashu.arora@sourcebits.com>
2014-10-16 17:53:42 +0530

    Superadmin_list
    pfa

---
+++ application/views/superadmin/users.php
@@ -0,0 +1,17 @@
+<div class="main_content">
+	<div class='content_settings' style='padding:3% 28%;'>
+		<legend style='border:1px solid #c6c6c6;border-bottom:none;'>
+           <h1>Emails Setup</h1>
+           <?php if(!isset($user_list) || empty($user_list)) { ?>
+           		<div class='red'>No Users present</div>
+           <?php }?>
+           <?php if(isset($user_list)) {
+           		foreach($user_list as $user) { ?>
+    			<div class='email_tab'>
+		    		<input type='text' value='<?php echo $user['email'];?>' id='' class='' name='' />
+		    		<a class="btn btn-danger right delete" href='<?php echo site_url("superadmin/delete/".$user['id']);?>'>Delete</a>
+		    	</div>
+           	<?php } }?>
+        </legend>
+    </div>
+</div>
